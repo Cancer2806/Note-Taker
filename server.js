1b2e1e6f973c1378ca4fd330d152039229511b96
Frank Lavery <frank.lavery@westnet.com.au>
2022-04-30 19:33:15 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,25 @@
+// Required Modules
+const express = require('express');
+const apiRouter = require('./routes/api');
+const webRouter = require('./routes/web');
+
+
+// configure
+const app = express();
+const PORT = 3001; // Change to Heroku required format when ready to deploy
+
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json());
+
+app.use(webRouter);
+app.use(apiRouter);
+
+
+console.log(`Directory Name is ${__dirname}`);
+
+// open port
+app.listen(PORT, function () {
+  console.log(`Server is currently running on https://localhost:${PORT}`);
+});
